7156e85c77c005dfc6f48ca64ee7b8cff095d97d
Mark Anthony Espiritu <mark.a.v.espiritu@gmail.com>
2023-05-15 16:16:22 +0800

    updated contract report

--- common/modules/v1/views/pr/reports/contract.php
+++ common/modules/v1/views/pr/reports/contract.php
@@ -45,14 +45,7 @@ $asset = AppAsset::register($this);
 <br>
 <p style="text-indent: 50px;">This agreement made and entered into this <?= date("F j, Y", strtotime($contractModel->po_date)) ?> by and between the <b><?= $agency->value ?> REGIONAL OFFICE 1</b>, represented by the Regional Director, <b><?= $rd->value ?></b>, hereinafter known as the party of the first part and <b><?= $supplier->business_name ?></b> represented by <b><?= $supplier->owner_name ?></b>, hereinafter known as the party of the second part.</p>
 <p class="text-center"><b>W I T N E S S E T H:</b></p>
-<p style="text-indent: 50px;">1. That the Party of the Second Part shall provide the following:</p>
-<p style="text-indent: 50px;"><b><?= $model->purpose ?>:</b></p>
 <span style="text-indent: 50px;"><?= $contractModel->content ?></span>
-<p style="text-indent: 50px;">2. That the Party of the First Part shall pay the Party of the Second Part in Philippine Currency the amount of <b><?= strtoupper(Yii::$app->controller->module->getNumberToWords(sprintf('%0.2f',$total['total']))) ?> (Php <?= number_format($total['total'], 2) ?>) ONLY</b> upon satisfactory completion of the service contracted for.</p>
-<p style="text-indent: 50px;">3. That this Contract shall automatically cease to be of any force and effect when sooner terminated at
-the option of any or both parties. In such case, payment shall be made on the basis of percentage of service
-completed.</p>
-<p style="text-indent: 50px;"><b>IN WITNESS WHEREOF</b>, both parties sign this Agreement this <b>17th day of October 2022</b> at the <?= ucwords(strtolower($agency->value))?> Regional Office 1, <?= $address->value ?>.</p>
 <table style="width: 100%">
     <tr>
         <td style="width: 50%;">
